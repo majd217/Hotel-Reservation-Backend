5b739958d88cb13976d4465de7c4fc58266191cf
majd217 <m.amireh@gju.edu.jo>
2024-03-04 22:33:07 +0300

    Login functionality enhanced

--- src/main/java/com/majdamireh/resvsystem/login/LoginEndPoint.java
+++ src/main/java/com/majdamireh/resvsystem/login/LoginEndPoint.java
@@ -1,30 +1,39 @@
 package com.majdamireh.resvsystem.login;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
+import org.springframework.ws.transport.http.HttpServletConnection;
 
 @Endpoint
 public class LoginEndPoint {
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @PayloadRoot(namespace = "http://hotelreservation.com/user", localPart = "UserLoginRequest")
-    @ResponsePayload
-    public com.hotelreservation.user.UserLoginResponse loginUser(@RequestPayload com.hotelreservation.user.UserLoginRequest loginRequest) {
-        com.hotelreservation.user.UserLoginResponse response = new com.hotelreservation.user.UserLoginResponse();
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        response.setSuccess(HttpStatus.OK.toString());
-        return response;
-    }
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@PayloadRoot(namespace = "http://hotelreservation.com/user", localPart = "UserLoginRequest")
+	@ResponsePayload
+	public void loginUser(@RequestPayload com.hotelreservation.user.UserLoginRequest loginRequest) throws IllegalArgumentException {
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		TransportContext ctx = TransportContextHolder.getTransportContext();
+		HttpServletRequest req = ((HttpServletConnection) ctx.getConnection()).getHttpServletRequest(); //if ur certain its http
+		HttpSession session = req.getSession();
+		
+		session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+				SecurityContextHolder.getContext());
+	}
 }
 
--- src/main/java/com/majdamireh/resvsystem/user/User.java
+++ src/main/java/com/majdamireh/resvsystem/user/User.java
@@ -5,90 +5,82 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-
-    @Column(name = "user_name")
-    private String userName;
-    @Column(name = "password")
-
-    private String password;
-
-    //generate a new sequence number at each entity
-    @SequenceGenerator(
-            name = "users_id_seq",
-            sequenceName = "users_id_seq",
-            allocationSize = 1
-    )
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "users_id_seq"
-    )
-    private Long id;
-    @Column(name = "email")
-
-    private String email;
-
-    protected User() {
-    }
-
-    public User(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public User(String userName, String password, String email) {
-        this.userName = userName;
-        this.password = password;
-        this.email = email;
-    }
-
-    public String getUserName()
-    {
-        return userName;
-    }
-
-    public void setUserName(String userName)
-    {
-        this.userName = userName;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", id=" + id +
-                ", email='" + email + '\'' +
-                '}';
-    }
+	
+	//generate a new sequence number at each entity
+	@SequenceGenerator(
+			name = "users_id_seq",
+			sequenceName = "users_id_seq",
+			allocationSize = 1
+	)
+	@Id
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "users_id_seq"
+	)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "user_name")
+	private String userName;
+	@Column(name = "password")
+	
+	private String password;
+	@Column(name = "email")
+	private String email;
+	
+	protected User() {
+	}
+	
+	public User(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	public User(String userName, String password, String email) {
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "User{" +
+				"userName='" + userName + '\'' +
+				", password='" + password + '\'' +
+				", id=" + id +
+				", email='" + email + '\'' +
+				'}';
+	}
 }
